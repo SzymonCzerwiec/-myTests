d52bcfb3e244054233f84505ff2d57afa7a9aa70
SzymonCzerwiec <72661406+SzymonCzerwiec@users.noreply.github.com>
2020-11-25 10:06:15 +0100

    Update testForNewWindow.java

--- testForNewWindow.java
+++ testForNewWindow.java
@@ -1,4 +1,4 @@
-package com.in28minutes.webDriver.scenarios;
+package com.in28minutes.webDriver.scenarios;// cwicze sobie tutaj
 
 import org.openqa.selenium.By;
 import org.testng.annotations.Test;
